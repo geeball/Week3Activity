6cdd4e41d2fac84d0f2f471c1b794abd9f9462b9
Geeball <U1759657@Unimail.hud.ac.uk>
2017-11-15 10:21:31 +0000

    greetings

---
+++ football_team_program.py
@@ -0,0 +1,3 @@
+total_pupils = 172
+number_of_players = total_pupils / 5
+players_per_team = 5
---
+++ integer_input_program.py
@@ -0,0 +1,7 @@
+IDEAL_MARK = (40)
+
+
+if student_mark >= IDEAL_MARK:
+    print('Nice Job! You Passed!')
+else:
+    print('Tough Luck, You Failed.')
