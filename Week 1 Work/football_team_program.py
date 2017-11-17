7ee9576a44ab6380cbbe5da4c8bcdbc8802047f3
Geeball <U1759657@Hud.ac.uk>
2017-11-17 15:49:14 +0000

    greetings

---
+++ Week 1 Work/football_team_program.py
@@ -0,0 +1,9 @@
+total_pupils = 172
+players_per_team = 5
+number_of_players = total_pupils // players_per_team   # // = "whole parts"
+inactive_players = total_pupils % number_of_players    # % = "remainder"
+
+print (total_pupils)
+print (number_of_players)
+print (inactive_players)
+
